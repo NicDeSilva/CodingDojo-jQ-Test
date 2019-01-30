7af158780c752d5d04bb20894de7bb1b498c759b
Vlad Dziun <v.dziun@gmail.com>
2019-01-30 12:17:12 -0800

    test1

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 
 $(document).ready(function(){
-$( "button" ).click(function() {
-    $(("div").children("p") ).addClass( "red" );
+// $( "button" ).click(function() {
+//     $(("div").children("p") ).addClass( "red" );
+//   });
+
+  $( "button" ).click(function() {
+    $( this ).slideToggle();
+    console.log("slide;")
   });
 });
